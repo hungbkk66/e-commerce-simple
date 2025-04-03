36479992cb35b51c382e1625edeba9fe06960a9c
hungbkk66 <laiviethung2003>
2025-04-03 10:02:20 +0700

    create logout

--- ecomm-dashboard/src/Component/Header.js
+++ ecomm-dashboard/src/Component/Header.js
@@ -1,8 +1,17 @@
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 function Header() {
+    let user = JSON.parse(localStorage.getItem('user-info'))
+
+    const navigate = useNavigate();
+    function logOut() {
+        localStorage.clear()
+        navigate("/register");
+    }
+
+
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
@@ -22,11 +31,13 @@ function Header() {
                     }
 
                 </Nav>
-                <Nav>
-                    <NavDropdown title="User Name">
-
-                    </NavDropdown>
-                </Nav>
+                {localStorage.getItem('user-info') ?
+                    <Nav>
+                        <NavDropdown title={user && user.name}>
+                            <NavDropdown.Item onClick={logOut}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    : null}
             </Navbar>
         </>
     )
